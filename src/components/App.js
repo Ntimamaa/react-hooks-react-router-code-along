2c0ac4b241e428128ba78347cef9d660c23c13a6
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-07-25 14:02:54 +0300

    Completed Assignment

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./Nav";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+
+function App(){
+    return(
+        <div>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default App;
